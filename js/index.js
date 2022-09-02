c95b5d696479b55ed3878c5969b3312cb88d3e85
mostafijurmrs <mostafijurrahmanmrs241@gmail.comm>
2022-09-03 01:16:21 +0600

    added a js file with API

--- js/index.js
+++ js/index.js
@@ -1,8 +1,25 @@
 
 
-const loadNewsCatagories = async () => {
+const loadTheCatagories = async () => {
     const url = `https://openapi.programming-hero.com/api/news/categories`;
     const res = await fetch(url);
     const data = await res.json();
-    disNewsCatagories(data.data.news_category);
+    displayTheCategories(data.data.news_category);
 }
+
+const displayTheCategories = categories => {
+    console.log(categories);
+    const categoriesContainer = document.getElementById('categories-container')
+    categories.forEach(category => {
+        console.log(category);
+
+        const newCategoriesDiv = document.createElement('div');
+        newCategoriesDiv.classList.add('news-category-name');
+        newCategoriesDiv.innerHTML = `
+        <p onclick="load" class="mx-7 hover:bg-indigo-100 hover:text-indigo-600 px-2 rounded font-medium">${category.category_name}</p>
+        
+        `;
+        categoriesContainer.appendChild(newCategoriesDiv);
+    });
+}
+loadTheCatagories();
