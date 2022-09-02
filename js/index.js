532fca37c05aba848db55cf5ad51a681d3bf646d
mostafijurmrs <mostafijurrahmanmrs241@gmail.comm>
2022-09-03 02:31:31 +0600

    added an another js function add with api

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 
-
 const loadTheCatagories = async () => {
     const url = `https://openapi.programming-hero.com/api/news/categories`;
     const res = await fetch(url);
@@ -11,21 +10,29 @@ const displayTheCategories = (categories) => {
     console.log(categories);
     const categoriesContainer = document.getElementById('categories-container')
     categories.forEach(category => {
-        console.log(category);
+        // console.log(category);
 
         const newCategoriesDiv = document.createElement('div');
         newCategoriesDiv.classList.add('news-category-name');
         newCategoriesDiv.innerHTML = `
-        <p onclick="displayNewsCard('${category.category_id}')" class="mx-7 hover:bg-indigo-100 hover:text-indigo-600 px-2 rounded font-medium">${category.category_name}</p>
+        <p onclick="loadNewsCard('${category.category_id}')" class="mx-7 hover:bg-indigo-100 hover:text-indigo-600 px-2 rounded font-medium">${category.category_name}</p>
         
         `;
         categoriesContainer.appendChild(newCategoriesDiv);
     });
 }
 
-const displayNewsCard = async () => {
-
+const loadNewsCard = async (category_id) => {
+    const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
+    const res = await fetch(url);
+    const data = res.json();
+    console.log(data);
 }
 
+const displayNewsCard = (cards) => {
+    cards.forEach(card => {
+        console.log(card);
+    })
+}
 
 loadTheCatagories();
