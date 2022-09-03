9b663f4f31d93ff15afba9b18fc791127f8ef7cf
mostafijurmrs <mostafijurrahmanmrs241@gmail.comm>
2022-09-04 00:09:03 +0600

    revision and modify the html file

--- js/index.js
+++ js/index.js
@@ -193,7 +193,7 @@ const displayModalDetails = (modalNews) => {
                             </div>
                         </div>
                         <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                            <button onclick="closeModal()" type="button" class="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm">Close</button>
+                            <button onclick="closeModal()" type="button" class="inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"><i class="fa-solid fa-xmark"></i></button>
                         </div>
                         </div>
                     </div>
