2f1d50b73cc9c6f220285ffdcd9fbedefe9b6426
mostafijurmrs <mostafijurrahmanmrs241@gmail.comm>
2022-09-03 13:04:24 +0600

    added the dainamically card design with javascript

--- js/index.js
+++ js/index.js
@@ -1,13 +1,14 @@
 
-const loadTheCatagories = async () => {
+const loadDataCatagories = async () => {
     const url = `https://openapi.programming-hero.com/api/news/categories`;
     const res = await fetch(url);
     const data = await res.json();
-    displayTheCategories(data.data.news_category);
+    displayDataCategories(data.data.news_category);
 }
 
-const displayTheCategories = (categories) => {
-    console.log(categories);
+const displayDataCategories = (categories) => {
+    // console.log(categories);
+
     const categoriesContainer = document.getElementById('categories-container')
     categories.forEach(category => {
         // console.log(category);
@@ -25,15 +26,16 @@ const loadNewsCard = async (category_id) => {
     const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
     const res = await fetch(url);
     const data = await res.json();
-    displayNewsCard(data.data);
+    loadDisplayNewsCard(data.data);
 }
 
-const displayNewsCard = (cards) => {
+const loadDisplayNewsCard = (cards) => {
     const newsCardContainer = document.getElementById('news-card-comtainer');
     newsCardContainer.innerHTML = '';
 
     cards.forEach(card => {
         console.log(card);
+
         const newCardDiv = document.createElement('div');
         newCardDiv.classList.add('newsCard');
         newCardDiv.innerHTML = `
@@ -42,9 +44,9 @@ const displayNewsCard = (cards) => {
             <img src=${card.image_url} alt="">
             </div>
             <div class="p-5 w-3/5">
-                <h1 class="text-xl font-bold">${card.title ? card.title : 'Title is Not Found Here'}.</h1>
-                <p class="my-4 mr-10 overflow-hidden">
-                    ${card.details.length > 150 ? card.details.slice(0, 300) + '...' : 'Details is Not Found Here'}
+                <h1 class="text-2xl font-bold">${card.title ? card.title : 'Title is Not Found Here'}.</h1>
+                <p class="my-4 font-italic hover:font-not-italic mr-10 overflow-hidden">
+                    ${card.details.length > 100 ? card.details.slice(0, 400) + '...' : 'Details is Not Found Here'}
                 </p>
                 <div class="flex items-end">
                     <div class="grid items-center grid-cols-2">
@@ -53,15 +55,18 @@ const displayNewsCard = (cards) => {
                                 <img class="w-10 h-10 rounded-full mr-4" src=${card.author.img} alt="Image is Not Found Here">
                             </div>
                             <div>
-                                <p class=" font-bold">
-                                    ${card.author.name ? card.author.name : 'Author Name not Found!'}
+                                <p class=" text-2xl font-semibold">
+                                    ${card.author.name ? card.author.name : 'Author Name  is not Found!'}
+                                </p>
+                                <p class=" text-xl">
+                                    ${card.author.published_date.length > 11 ? card.author.published_date.slice(0, 10) : 'This news published date is not Found!'}
                                 </p>
                             </div>
                         </div>
 
                         <img src="images/carbon_view.png" alt="">
                         <div>
-                            <p class="ml-5 font-bold">${card.total_view ? card.total_view : 'No view Data found'}</p>
+                            <p class="ml-5 font-bold">${card.total_view ? card.total_view : 'View Data is Not Found'}</p>
                         </div> 
                     </div>
                 </div>
@@ -73,11 +78,11 @@ const displayNewsCard = (cards) => {
     })
 }
 const loadNewsDetails = async (news_id) => {
-    const url = `https:openapi.programming-hero.com/api/news/${news_id};`
+    const url = `https://openapi.programming-hero.com/api/news/${news_id};`
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data.data[0]);
+    // console.log(data.data[0]);
 
 }
 
-loadTheCatagories();
+loadDataCatagories();
